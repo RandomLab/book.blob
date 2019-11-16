eb2641696aa2d03068cfd44821f1467edd8a820a
Corentin Brulé <corentin.brule@gmail.com>
2019-11-16 19:21:18 +0100

    postprocess pour la justification des nom des auteurs et la mise en place des notes de bas page (pour le moment manuellement

--- dist/public/js/postprocess.js
+++ dist/public/js/postprocess.js
@@ -2,6 +2,10 @@ function show() {
   pagedjs();
 }
 
+$(document).ready(function(){
+  /*hardJustify();*/
+});
+
 function image_container(){
   images = document.querySelectorAll("p img");
   for(var i = 0; i < images.length ;  i++){
@@ -25,6 +29,27 @@ function interface(){
   }
 }
 
+function hardJustify(){
+  $(".author").each(function(i, span){
+    var inject = '';
+    console.log(span)
+    var letters = span.innerText
+    console.log(letters)
+    for(var j=0; j<letters.length;j++){
+        inject += '<span class=char_"'+(j)+'" aria-hidden="true">'+letters[j]+'</span>';
+    }
+    span.innerHTML = inject;
+  });
+}
+
+function veuves(){
+  $("p").each(function(i, para){
+      console.log(para);
+      para.innerHTML = para.innerHTML.replace(/(.*) (.*)/g,"$1&nbsp;$2");
+  });
+}
+
+/*
 function footnote(){
   $(".pagedjs_page_content").each(function(i, page){
       notes = page.getElementsByClassName("note");
@@ -36,6 +61,25 @@ function footnote(){
           }
       }
   });
+}
+*/
+
+function footnote(){
+  $(".pagedjs_page_content").each(function(i, page){
+      $(page).find(".note").wrapAll("<div class='notes'/>");
+  });
+  console.log($(".pagedjs_page_content .note"));
+  /*$(".pagedjs_page_content").each(function(i, page){
+      notes = page.getElementsByClassName("note");
+
+      console.log(notes);
+      for (var j = notes.length-1; j > -1 ; j--){
+          console.log(notes[j])
+          if (j < notes.length-1){
+              notes[j].style.top = notes[j+1].offsetTop - notes[j+1].offsetHeight;
+          }
+      }
+  });*/
 }
 
 image_container();
