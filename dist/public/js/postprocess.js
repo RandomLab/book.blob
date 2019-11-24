e82639bd1ff921348a010532266c7a30798a93a5
Corentin Brulé <corentin.brule@gmail.com>
2019-11-24 18:31:10 +0100

    reorganisation des scripts et lancement de pagedjs à la fin de hyphenopoly

--- dist/public/js/postprocess.js
+++ dist/public/js/postprocess.js
@@ -1,18 +1,3 @@
-function show() {
-  pagedjs();
-}
-
-$(document).ready(function(){
-  /*hardJustify();*/
-});
-
-function image_container(){
-  images = document.querySelectorAll("p img");
-  for(var i = 0; i < images.length ;  i++){
-    image = images[i]
-    image.parentElement.classList.add("image_container");
-  }
-}
 
 console.log("postprocess");
 
@@ -28,59 +13,3 @@ function interface(){
     page.appendChild(span)
   }
 }
-
-function hardJustify(){
-  $(".author").each(function(i, span){
-    var inject = '';
-    console.log(span)
-    var letters = span.innerText
-    console.log(letters)
-    for(var j=0; j<letters.length;j++){
-        inject += '<span class=char_"'+(j)+'" aria-hidden="true">'+letters[j]+'</span>';
-    }
-    span.innerHTML = inject;
-  });
-}
-
-function veuves(){
-  $("p").each(function(i, para){
-      console.log(para);
-      para.innerHTML = para.innerHTML.replace(/(.*) (.*)/g,"$1&nbsp;$2");
-  });
-}
-
-/*
-function footnote(){
-  $(".pagedjs_page_content").each(function(i, page){
-      notes = page.getElementsByClassName("note");
-      console.log(notes);
-      for (var j = notes.length-1; j > -1 ; j--){
-          console.log(notes[j])
-          if (j < notes.length-1){
-              notes[j].style.top = notes[j+1].offsetTop - notes[j+1].offsetHeight;
-          }
-      }
-  });
-}
-*/
-
-function footnote(){
-  $(".pagedjs_page_content").each(function(i, page){
-      $(page).find(".note").wrapAll("<div class='notes'/>");
-  });
-  console.log($(".pagedjs_page_content .note"));
-  /*$(".pagedjs_page_content").each(function(i, page){
-      notes = page.getElementsByClassName("note");
-
-      console.log(notes);
-      for (var j = notes.length-1; j > -1 ; j--){
-          console.log(notes[j])
-          if (j < notes.length-1){
-              notes[j].style.top = notes[j+1].offsetTop - notes[j+1].offsetHeight;
-          }
-      }
-  });*/
-}
-
-image_container();
-show();
--- dist/public/js/preprocess.js
+++ dist/public/js/preprocess.js
@@ -1,9 +1,61 @@
+/*config Hyphenopoly*/
+var Hyphenopoly = {
+    require: {
+        "fr": "FORCEHYPHENOPOLY"
+    },
+    setup: {
+      dontHyphenate: {
+        h6: true,
+        cite: true,
+        figcaption: true
+      },
+      minWordLength: 12
+    },
+    paths: {
+         patterndir: "./js/hyphens/patterns/",
+         maindir: "./js/hyphens/"
+     },
+    selectors: {
+        ".hyphenate": {
+            minWordLength: 12
+        }
+    }
+};
 
 console.log("preprocess");
 
-function pagedjs() {
-  var script = document.createElement("script");
-  script.type = "text/javascript";
-  script.src = "js/paged.polyfill.js";
-  document.getElementsByTagName("head")[0].appendChild(script);
+/* do things on the HTML doc before pagedjs */
+
+function image_container(){ // juste add class on image_container for style
+  images = document.querySelectorAll("p img");
+  for(var i = 0; i < images.length ;  i++){
+    image = images[i]
+    image.parentElement.classList.add("image_container");
+  }
 }
+
+function hard_justify(){
+  $(".author").each(function(i, span){
+    var inject = '';
+    //console.log(span)
+    var letters = span.innerText
+    //console.log(letters)
+    for(var j=0; j<letters.length;j++){
+        inject += '<span class=char_"'+(j)+'" aria-hidden="true">'+letters[j]+'</span>';
+    }
+    span.innerHTML = inject;
+  });
+}
+
+function veuves(){
+  $("p").each(function(i, para){
+      //console.log(para);
+      para.innerHTML = para.innerHTML.replace(/(.*) (.*)/g,"$1&nbsp;$2");
+  });
+}
+
+$(document).ready(function(){
+  image_container();
+  hard_justify();
+  veuves();
+});
