bd2dd1f7a60d66af00104bb5ef4a5655eed988f1
Corentin Brulé <corentin.brule@gmail.com>
2020-01-29 13:48:33 +0100

    passage de la numérotation des notes dans un élement ::before

--- dist/public/js/process.js
+++ dist/public/js/process.js
@@ -33,12 +33,11 @@ function footnote(page){
     var chap = $(page).find(".subchapter").attr("data-id")
     if(chap){
       if($(page).find(".notes").height() > $(page).find(".pagedjs_area").height()){
+
+        // passage de la page de droite à celle de gauche
         if($(page).hasClass("pagedjs_right_page")){
-          //console.log($(page))
           var num_left_page = $(page).attr("data-page-number")-1;
-          //console.log(num_left_page);
           var left_page = $("#page-"+num_left_page.toString());
-          //console.log(left_page);
           var new_note = $("<span class='note'></span>")
           var i = 0;
 
@@ -58,14 +57,22 @@ function footnote(page){
               $(notesOverflow).append("<span class='note'></span>")
             }
           }
-          console.log(new_note)
+          //console.log(new_note)
           if(left_page.find(".notes").length > 0){
             left_page.find(".notes").append(new_note);
           }else{
             left_page.find(".subchapter").append("<span class=notes>"+new_note[0].outerHTML+"</span>")
           }
+          $(left_page).find(".note").each(function(){
+            first_word = $(this).text().split(" ")[0];
+            if (/\d{1,2}/.test(first_word)){
+              $(this).html($(this).html().replace(first_word+" ", ""));
+              $(this).attr("data-count",first_word);
+            }
+          });
         }
 
+        // mise en overflow des notes en trop sur la page de gauche
         if($(page).hasClass("pagedjs_left_page") && $(page).find(".notes").height() > $(page).find(".pagedjs_area").height()+lineheight){
           var notesOverflow = $("<span class='notesOverflow'></span>");
 
@@ -73,22 +80,29 @@ function footnote(page){
           var i = 0;
 
           while($(page).find(".notes").height() > $(page).find(".pagedjs_area").height()+lineheight){
-            var first_note = $(page).find(".notes").children()[i]
+            var first_note = $(page).find(".notes").children()[i];
             var words = $(first_note).text().split(" ");
             var word_to_move = words[0];
             $(first_note).text(words.slice(1).join(" "));
-            console.log($(notesOverflow).children()[i]);
-            var notesOverflow_text = $(notesOverflow.children()[i]).text() + word_to_move + " "
-            $(notesOverflow.children()[i]).text(notesOverflow_text)
+            //console.log($(notesOverflow).children()[i]);
+            var notesOverflow_text = $(notesOverflow.children()[i]).text() + word_to_move + " ";
+            $(notesOverflow.children()[i]).text(notesOverflow_text);
             if (words.length == 1){
               i ++;
-              $(notesOverflow).append("<span class='note'></span>")
+              $(notesOverflow).append("<span class='note'></span>");
             }
           }
-          $(page).find(".subchapter").append(notesOverflow)
+          $(page).find(".subchapter").append(notesOverflow);
         }
       }
     }
+    $(page).find(".note").each(function(){
+      first_word = $(this).text().split(" ")[0];
+      if (/\d{1,2}/.test(first_word)){
+        $(this).html($(this).html().replace(first_word+" ", ""));
+        $(this).attr("data-count",first_word);
+      }
+    });
     /* in the case where the heigh of the page_content is manually changed
     margin_bottom_height = $(page).parent().parent().find(".pagedjs_margin-bottom").height()
     if (margin_bottom_height > 60){
